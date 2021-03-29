f542b6d55bb97addac10c7990c7931b38651e1fa
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-03-29 17:13:04 +0200

    init-hooks-from-landing

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par DonkeyGeek - 2020</p>
+                <p>Les icônes Wolverine, Spider-Man, Spider-Man, Iron-Man</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
