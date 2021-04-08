7da4c15e84e029ae9b4368ceca85493efb115ce6
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-08 18:19:43 +0200

    import useContext From firebase

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { FirebaseContext } from '../Firebase';
 
 const Logout = () => {
+
+    // mise en place du context
+    const firebase = useContext(FirebaseContext);
+
+    // création de la variable d'état
+    const [checked, setChecked] = useState(false);
+
+    /*vérification si l'utilisateur est connecté
+    grace au usEffect on peut changer le state*/
+    useEffect(() => {
+        if (checked) {
+            console.log("Déconnexion");
+            // on charge la fonction du composant issu de la class firebase
+            firebase.signoutUser();
+        }
+        // ici on place les dépendances
+    }, [checked, firebase]);
+
+    const handleChange = event => {
+        setChecked(event.target.checked)
+    }
+
     return (
         <div className="logoutContainer">
+            {/* création du switch */}
             <label className="switch">
-                <input type="checkbox"
+                <input
+                    // appel de la variable d'état
+                    onChange={handleChange}
+                    type="checkbox"
+                    // appel de la variable d'état
+                    checked={checked}
                 />
                 <span className="slider round"></span>
             </label>
-            LOGOUT
         </div>
     )
 }
