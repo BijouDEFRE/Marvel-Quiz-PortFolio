c269d696f19a8502219446f50abd6ea8eaf94712
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-08 16:45:17 +0200

    error message and connexion

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../Firebase';
 
-const Login = () => {
+const Login = (props) => {
+
+    const firebase = useContext(FirebaseContext);
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [btn, setBtn] = useState(false);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         if (password.length > 5 && email !== '') {
@@ -23,6 +27,23 @@ const Login = () => {
     /* version avec le setter directement invoqué dans une fonction fléchée */
     /* <input onChange={event => setPassword(event.target.value)} value={password}..... */
 
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        firebase.loginUser(email, password)
+        .then(user => {
+            console.log(user);
+            setEmail('');
+            setPassword('');
+            props.history.push('/welcome')
+        })
+        .catch(error => {
+            setError(error);
+            setEmail('');
+            setPassword('');
+        })
+    }
+
     return (
         <div className="signUpLoginBox">
             <div className="slContainer">
@@ -30,9 +51,11 @@ const Login = () => {
                 </div>
                 <div className="formBoxRight">
                     <div className="formContent">
+
+                        {error !== '' && <span>{error.message}</span>}
                         
                         <h2>Connexion</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
 
                             <div className="inputBox">
                                 <input onChange={event => setEmail(event.target.value)} value={email} type="email" autoComplete="off" required />
