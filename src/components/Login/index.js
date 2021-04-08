b9b1fa365f7c1ec8093b458627f0d3f534fc9d0a
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-08 16:36:02 +0200

    create submit on Firebase

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [btn, setBtn] = useState(false);
+
+    useEffect(() => {
+        if (password.length > 5 && email !== '') {
+            setBtn(true)
+        } else if (btn) {
+            setBtn(false)
+        }
+    }, [password, email, btn])
+    
+    /* version avec une fonction en dehors */
+    /*const handleEmail = event => {
+        setEmail(event.target.value);
+    } */
+    
+    /* version avec le setter directement invoqué dans une fonction fléchée */
+    /* <input onChange={event => setPassword(event.target.value)} value={password}..... */
+
     return (
         <div className="signUpLoginBox">
             <div className="slContainer">
-                LOGIN
+            <div className="formBoxLeftLogin">
+                </div>
+                <div className="formBoxRight">
+                    <div className="formContent">
+                        
+                        <h2>Connexion</h2>
+                        <form>
+
+                            <div className="inputBox">
+                                <input onChange={event => setEmail(event.target.value)} value={email} type="email" autoComplete="off" required />
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="inputBox">
+                                <input onChange={event => setPassword(event.target.value)} value={password} type="password" autoComplete="off" required />
+                                <label htmlFor="password">Mot de passe</label>
+                            </div>
+
+                            { btn ? <button>Connexion</button> : <button disabled>Connexion</button> }
+
+                        </form>
+                        <div className="linkContainer">
+                            <Link className="simpleLink" to="/signup">Nouveau sur Marvel Quiz ? Insrivez-vous maintenant.</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
