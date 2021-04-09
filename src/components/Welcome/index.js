446e7d3648319e79b3bc5ec7e268a4c71ca05003
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-09 11:18:08 +0200

    add listener from Firebase

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,29 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useContext, useEffect } from 'react';
+import { FirebaseContext } from '../Firebase';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
 
-const Welcome = () => {
+// on passe le "props" pour accéder à history et push
+const Welcome = props => {
+
+    // chargement du useContext de firebase
+    const firebase = useContext(FirebaseContext);
 
     const [userSession, setUserSession] = useState(null);
 
-    // const display = userSession ? () : ()
+    useEffect(() => {
+        // methode "listener" fournie par Firebase qui permet de détecter un user identifier
+        let listener = firebase.auth.onAuthStateChanged(user => {
+            // condition : pas de user identifié, je redirige vers la page /
+            user ? setUserSession(user) : props.history.push('/')
+        });
+
+        return () => {
+            // on arrête le composant
+            listener()
+        }
+        // pas besoin de dépendance donc [] vide
+    }, [])
 
     return userSession === null ? (
         <Fragment>
