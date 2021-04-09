49c9d57497bdd3036c7c0ce23c696d80c95e1781
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-09 11:05:32 +0200

    create loader

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
 
 const Welcome = () => {
-    return (
+
+    const [userSession, setUserSession] = useState(null);
+
+    // const display = userSession ? () : ()
+
+    return userSession === null ? (
+        <Fragment>
+            <div className="loader"></div>
+            <p>Loading...</p>
+        </Fragment>
+    ) : (
         <div className="quiz-bg">
             <div className="container">
                 <Logout />
                 <Quiz />
             </div>
-            
         </div>
     )
 }
