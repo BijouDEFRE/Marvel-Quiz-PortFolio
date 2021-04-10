68f90c35f1e0026644e754e09e13eb073138d0e3
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-10 15:24:46 +0200

    create elements

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -7,7 +7,8 @@ const ProgressBar = () => {
             <div className="progressPercent"> Question : 1/10</div>
             <div className="progressPercent"> Progression : 10%</div>
         </div>
-        <div>
+        <div className="progressBar">
+            <div className="progressBarChange" style={{width: '10%'}}></div>
         </div>
         </Fragment>
     )
--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -17,6 +17,12 @@ class Quiz extends Component {
                 {/* <h2>Pseudo: {pseudo}</h2> */}
                 <Levels />
                 <ProgressBar />
+                <h2>Notre Question Quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 2</p>
+                <p className="answerOptions">Question 3</p>
+                <p className="answerOptions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
             </div>
         )
     }
