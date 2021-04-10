1c4c15e0f90753bb821bf78a1c4cb07bf916b391
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-10 13:14:44 +0200

    recup userdata from BDD with 2 examples class&function

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Quiz = (props) => {
-    // console.log(props.userData.pseudo);
-    return (
-        <div>
-            <h2>Pseudo: {props.userData.pseudo}</h2>
-        </div>
-    )
+class Quiz extends Component {
+
+    render() {
+
+        /* je créer ma variable en dehors
+        pour une meilleur lisibilité dans le JSX
+        <h2>Pseudo: {this.props.userData.pseudo}</h2>
+        cela s'appelle le déstructuring */
+        const { pseudo } = this.props.userData;
+        // console.log(props.userData.pseudo);
+        return (
+            <div>
+                <h2>Pseudo: {pseudo}</h2>
+            </div>
+        )
+    }
 }
 
 export default Quiz;
