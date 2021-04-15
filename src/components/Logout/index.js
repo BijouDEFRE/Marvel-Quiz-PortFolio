6f1b74923c09409066705f3a66d94e3fa271bce6
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-15 09:47:48 +0200

    use react-tooltip

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { FirebaseContext } from '../Firebase';
+import ReactTooltip from 'react-tooltip';
 
 const Logout = () => {
 
@@ -35,8 +36,12 @@ const Logout = () => {
                     // appel de la variable d'état
                     checked={checked}
                 />
-                <span className="slider round"></span>
+                <span className="slider round" data-tip="Déconnexion"></span>
             </label>
+            <ReactTooltip
+            place="left"
+            effect="solid"
+            />
         </div>
     )
 }
