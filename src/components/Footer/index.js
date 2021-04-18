daba1cefb786e74497c529e612971bb4145d4495
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-04-18 20:01:12 +0200

    testdeploy4000

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer>
             <div className="footer-container">
                 <p>Projet réalisé grace à l'excellent tuto de DonkeyGeek - 2021<br></br>
-                    JE TESTE Les images des personnages marvel sont issus du site www.hiclipart.com</p>
+                    JE TESTE ET ENCORE ET ENCORE Les images des personnages marvel sont issus du site www.hiclipart.com</p>
             </div>
         </footer>
     )
